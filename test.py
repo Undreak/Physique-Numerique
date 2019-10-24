85a73690b8a2111290b9546cbc824510819c1d20
Undreak <alexandre.poke@live.fr>
2019-10-24 12:00:37 +0200

    hydrogen test tout a l'air de bien marcher

--- test.py
+++ test.py
@@ -3,5 +3,9 @@ import scipy as sp
 import matplotlib.pyplot as plt
 
 print('Hello World')
+x = np.linspace(0,10,100)
+y = np.sin(x)
+plt.plot(x,y)
+plt.show()
 #test de git.
-#nouveau test de git. 
+#nouveau test de git.
