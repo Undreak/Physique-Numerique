0148c4652f4000d88d4985e09bc4ac8af4d2fdfb
Undreak <alexandre.poke@live.fr>
2020-01-20 11:34:42 +0100

    update finale

--- vol.py
+++ vol.py
@@ -17,15 +17,9 @@ def vol(N,theta,tsim,m=29981,x0=0,z0=0,vx0=0,vz0=0,ax0=0,az0=0,Cx=0.5,S=np.pi*(0
     Ttab,rhotab = np.zeros(t),np.zeros(t)
     Ttab[0],rhotab[0] = atmosphere(z0)
     thetaI=theta
-    Cpara,Spara = 1.75,0    ## Coefficient de frottement et surface des parachutes, nulle au départ car pas déployé.
-    prop = q*ve*poussee              ## Force de poussée de la fusée
     
     for i in range(t-1):
         T, rho = atmosphere(z[i])  ## Calcul de la densité de l'air et de la température en fontion de l'altitude.
-        if vz[i] < 0:
-            Cx = coeff(vz[i],vx[i],T)    ## Calcul du coeff Cx lors de la descente
-            Spara = para(z[i])             ## et de Spara à partir d'une certaine altitude.
-
         if z[i] < 0:            ## On arrete le calcul quand la fusée touhe le sol et on ne garde que les dernières données calculé
             x,z,m,vx,vz,ax,az,Ttab,rhotab = x[:i],z[:i],m,vx[:i],vz[:i],ax[:i],az[:i],Ttab[:i],rhotab[:i]
             break
@@ -33,21 +27,18 @@ def vol(N,theta,tsim,m=29981,x0=0,z0=0,vx0=0,vz0=0,ax0=0,az0=0,Cx=0.5,S=np.pi*(0
         rhotab[i+1] = rho
         if thetaF != 0 and i <= 24/dt:
             theta -= (thetaI - thetaF)*dt/24    ## Calcul de la variation d'angle, pi/2 => pi/4 en 24 secondes
-        #ax[i+1] = ( prop - 0.5*rho*abs(vx[i])*vx[i]*(S*Cx + Spara*Cpara))*np.cos(theta)/m
-        #az[i+1] = ( prop - 0.5*rho*abs(vz[i])*vz[i]*(S*Cx + Spara*Cpara))*np.sin(theta)/m - g
 
         if poussee == 1:    ## Calcul de la variation de masse
             m -= q*dt
-        if m <= mcap:       ## Simple précaution pour éviter que le programme ne fasse n'importe quoi
-            prop = 0
+
         ##Calcul de la vitesse par la méthode de Runge-Kutta d'ordre 4
-        k1 = acceleration(z[i],vx[i],vz[i],theta,m,poussee,N)
+        k1 = acceleration(z[i],vx[i],vz[i],theta,m,poussee)
         vx1,vz1 = vx[i]+k1[0]*dt/2 , vz[i]+k1[1]*dt/2
-        k2 = acceleration(z[i],vx1,vz1,theta,m,poussee,N)
+        k2 = acceleration(z[i],vx1,vz1,theta,m,poussee)
         vx2,vz2 = vx[i]+k2[0]*dt/2 , vz[i]+k2[1]*dt/2
-        k3 = acceleration(z[i],vx2,vz2,theta,m,poussee,N)
+        k3 = acceleration(z[i],vx2,vz2,theta,m,poussee)
         vx3,vz3 = vx[i]+k3[0]*dt , vz[i]+k3[1]*dt
-        k4 = acceleration(z[i],vx3,vz3,theta,m,poussee,N)
+        k4 = acceleration(z[i],vx3,vz3,theta,m,poussee)
         vx4,vz4 = vx[i]+dt*(k1[0]+2*k2[0]+2*k3[0]+k4[0])/6 , vz[i]+dt*(k1[1]+2*k2[1]+2*k3[1]+k4[1])/6
 
         vx[i+1] = vx4
@@ -56,21 +47,16 @@ def vol(N,theta,tsim,m=29981,x0=0,z0=0,vx0=0,vz0=0,ax0=0,az0=0,Cx=0.5,S=np.pi*(0
         x[i+1] = x[i] + vx[i]*dt
         z[i+1] = z[i] + vz[i]*dt
         
-        ax[i+1] = ( prop - 0.5*rho*abs(vx[i+1])*vx[i+1]*(S*Cx + Spara*Cpara))*np.cos(theta)/m
-        az[i+1] = ( prop - 0.5*rho*abs(vz[i+1])*vz[i+1]*(S*Cx + Spara*Cpara))*np.sin(theta)/m - g
+        ax[i+1],az[i+1] = acceleration(z[i+1],vx[i+1],vz[i+1],theta,m,poussee)
 
     return x,z,m,vx,vz,ax,az,Ttab,rhotab
 
-def acceleration(z,vx,vz,theta,m,poussee,N,S=np.pi*(0.89)**2,Cpara=1.75,Spara=0,Cx=0.5):
+def acceleration(z,vx,vz,theta,m,poussee,S=np.pi*(0.89)**2,Cpara=1.75,Spara=0,Cx=0.5):
     T, rho = atmosphere(z)  ## Calcul de la densité de l'air et de la température en fontion de l'altitude.
     if vz < 0:
         Cx = coeff(vz,vx,T)    ## Calcul du coeff Cx lors de la descente
         Spara = para(z)             ## et de Spara à partir d'une certaine altitude.
-    prop = q*ve
-    if poussee == 1:    ## Calcul de la variation de masse
-        m -= q/N
-    if m <= mcap:       ## Simple précaution pour éviter que le programme ne fasse n'importe quoi
-        prop = 0
+    prop = q*ve*poussee
     ax = ( prop - 0.5*rho*abs(vx)*vx*(S*Cx + Spara*Cpara))*np.cos(theta)/m
     az = ( prop - 0.5*rho*abs(vz)*vz*(S*Cx + Spara*Cpara))*np.sin(theta)/m - g
 
@@ -147,7 +133,7 @@ theta2 = np.pi/4
 tsim1 = 16
 tsim2 = 127.5
 tsim3 = 700
-N = 100
+N = 500
 
 ############## Calcul des trajectoires
 
@@ -161,8 +147,8 @@ x3,z3,m3,vx3,vz3,ax3,az3,Ttab3,rhotab3 = vol(N,theta2,4,x0=x2[-1],z0=z2[-1],m=m2
 x4,z4,m4,vx4,vz4,ax4,az4,Ttab4,rhotab4 = vol(N,np.pi*34/180,tsim3,x0=x3[-1],z0=z3[-1],m=1400,vx0=vx3[-1],vz0=vz3[-1],ax0=ax3[-1],az0=az3[-1])
 
 t1 = np.linspace(0,len(z1)/N,len(z1))
-t12 = np.linspace(t1[-1],40,2400)
-t2 = np.linspace(t12[-1],143.5,10350)
+t12 = np.linspace(t1[-1],40,len(z2[0:24*N]))
+t2 = np.linspace(t12[-1],143.5,len(z2[24*N:]))
 t3 = np.linspace(t2[-1],t2[-1]+4,len(z3))
 t4 = np.linspace(t3[-1],len(z1)/N+len(z2)/N+len(z3)/N+len(z4)/N,len(z4))
 
@@ -172,7 +158,11 @@ para2 = int(np.argwhere(z4<=3000)[0])
 
 #plt.plot(t1,az1) and plt.plot(t2,az2) and plt.plot(t3,az3) and plt.plot(t4,az4)
 #plt.plot(t1,ax1) and plt.plot(t2,ax2) and plt.plot(t3,ax3) and plt.plot(t4,ax4)
-plt.plot(t1,(az1**2 + ax1**2)**0.5) and plt.plot(t12,(az2[0:2400]**2 + ax2[0:2400]**2)**0.5) and plt.plot(t2,(az2[2400:]**2 + ax2[2400:]**2)**0.5) and plt.plot(t3,(az3**2 + ax3**2)**0.5) and plt.plot(t4,(az4**2 + ax4**2)**0.5)
+plt.plot(t1,(az1**2 + ax1**2)**0.5)
+plt.plot(t12,(az2[0:24*N]**2 + ax2[0:24*N]**2)**0.5)
+plt.plot(t2,(az2[24*N:]**2 + ax2[24*N:]**2)**0.5)
+plt.plot(t3,(az3**2 + ax3**2)**0.5)
+plt.plot(t4,(az4**2 + ax4**2)**0.5)
 plt.title('Accélération en fonction du temps')
 plt.xlabel('t (s)', color='tab:blue')
 plt.ylabel('a (m/s^2)', color='tab:red')
